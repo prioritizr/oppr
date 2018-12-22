bf426cb10ae2e4f309c069d9068ee23158461e0d
jeffreyhanson <jeffrey.hanson@uqconnect.edu.au>
2018-12-22 17:21:37 +1000

    lint c++ code

--- src/rcpp_apply_decisions.cpp
+++ src/rcpp_apply_decisions.cpp
@@ -10,11 +10,11 @@ bool rcpp_apply_decisions(SEXP x, std::string vtype, double default_lower,
                   ptr->_number_of_projects +
                   (ptr->_number_of_actions * ptr->_number_of_projects);
   // assign decisions
-  for (std::size_t i=0; i < n; ++i)
+  for (std::size_t i = 0; i < n; ++i)
     ptr->_vtype.push_back(vtype);
-  for (std::size_t i=0; i < n; ++i)
+  for (std::size_t i = 0; i < n; ++i)
     ptr->_lb.push_back(default_lower);
-  for (std::size_t i=0; i < n; ++i)
+  for (std::size_t i = 0; i < n; ++i)
     ptr->_ub.push_back(default_upper);
   // return success
   return true;
