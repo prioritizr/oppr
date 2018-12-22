9a527a23cb0534c1667afc4c0d440c0d993a3c9d
jeffreyhanson <jeffrey.hanson@uqconnect.edu.au>
2018-12-23 09:34:59 +1000

    fix min set obj cpp

--- src/rcpp_apply_min_set_objective.cpp
+++ src/rcpp_apply_min_set_objective.cpp
@@ -13,13 +13,23 @@ bool rcpp_apply_min_set_objective(SEXP x, Rcpp::List targets_list,
   // add objective function
   for (std::size_t i = 0; i < (ptr->_number_of_actions); ++i)
     ptr->_obj.push_back(costs[i]);
+  for (std::size_t i = 0;
+       i < (ptr->_number_of_projects) +
+           ((ptr->_number_of_projects) * (ptr->_number_of_features)) +
+           (ptr->_number_of_features); ++i) {
+    ptr->_obj.push_back(0.0);
+  }
 
   // add target constraints
   std::size_t r = A_original_nrow - 1;
   for (std::size_t f = 0; f < (ptr->_number_of_features); ++f) {
     ++r;
     ptr->_A_i.push_back(r);
-    ptr->_A_j.push_back(r);
+    ptr->_A_j.push_back(
+      (ptr->_number_of_actions) +
+      (ptr->_number_of_projects) +
+      ((ptr->_number_of_projects) * (ptr->_number_of_features)) +
+      f);
     ptr->_A_x.push_back(1.0);
     ptr->_sense.push_back(Rcpp::as<std::string>(targets_sense[f]));
     ptr->_rhs.push_back(targets_value[f]);
