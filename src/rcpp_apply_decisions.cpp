4a6b1eb840df9745762b693b719d9cc5b38f5783
jeffreyhanson <jeffrey.hanson@uqconnect.edu.au>
2018-12-23 09:34:50 +1000

    fix decisions cpp

--- src/rcpp_apply_decisions.cpp
+++ src/rcpp_apply_decisions.cpp
@@ -6,9 +6,10 @@ bool rcpp_apply_decisions(SEXP x, std::string vtype, double default_lower,
                           double default_upper) {
   Rcpp::XPtr<OPTIMIZATIONPROBLEM> ptr = Rcpp::as<Rcpp::XPtr<OPTIMIZATIONPROBLEM>>(x);
   // determine how many variables to assign decisions to based on formulation
-  std::size_t n = ptr->_number_of_actions +
-                  ptr->_number_of_projects +
-                  (ptr->_number_of_actions * ptr->_number_of_projects);
+  std::size_t n = (ptr->_number_of_actions) +
+                  (ptr->_number_of_projects) +
+                  (ptr->_number_of_projects * ptr->_number_of_features) +
+                  (ptr->_number_of_features);
   // assign decisions
   for (std::size_t i = 0; i < n; ++i)
     ptr->_vtype.push_back(vtype);
