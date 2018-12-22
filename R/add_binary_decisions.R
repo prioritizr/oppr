cf3a518ffda0f59d05eb78d5575491c240b38302
jeffreyhanson <jeffrey.hanson@uqconnect.edu.au>
2018-12-22 17:19:42 +1000

    add binary decisions

---
+++ R/add_binary_decisions.R
@@ -0,0 +1,43 @@
+#' @include internal.R Parameters-proto.R Decision-proto.R
+NULL
+
+#' Add binary decisions
+#'
+#' Add a binary decision to a project prioritization \code{\link{problem}}.
+#' This is the classic decision of either funding or not funding a management
+#' action.
+#'
+#' @param x \code{\link{ProjectProblem-class}} object.
+#'
+#' @details Project prioritization problems involve making decisions on
+#'   management actions that are grouped into projects.Only a
+#'   single decision should be added to a \code{ProjectProblem} object.
+#'   If no decision is added to a problem then this decision class will
+#'   be used by default. Currently, this is the only supported decision type.
+#'
+#' @return \code{\link{ProjectProblem-class}} object with the decisions
+#'   added to it.
+#'
+#' @seealso \code{\link{decisions}}.
+#'
+#' @examples
+#' #TODO
+#' @name add_binary_decisions
+NULL
+
+#' @rdname add_binary_decisions
+#' @export
+add_binary_decisions <- function(x) {
+  # assert argument is valid
+  assertthat::assert_that(inherits(x, "ProjectProblem"))
+  # add decision
+  x$add_decisions(
+    pproto("BinaryDecision",
+           Decision,
+           name = "Binary decision",
+           apply = function(self, x) {
+             assertthat::assert_that(inherits(x,
+                                     "OptimizationProblem"))
+             invisible(rcpp_apply_decisions(x$ptr, "B", 0, 1))
+           }))
+}
---
+++ src/rcpp_apply_decisions.cpp
@@ -0,0 +1,21 @@
+#include "package.h"
+#include "optimization_problem.h"
+
+// [[Rcpp::export]]
+bool rcpp_apply_decisions(SEXP x, std::string vtype, double default_lower,
+                          double default_upper) {
+  Rcpp::XPtr<OPTIMIZATIONPROBLEM> ptr = Rcpp::as<Rcpp::XPtr<OPTIMIZATIONPROBLEM>>(x);
+  // determine how many variables to assign decisions to based on formulation
+  std::size_t n = ptr->_number_of_actions +
+                  ptr->_number_of_projects +
+                  (ptr->_number_of_actions * ptr->_number_of_projects);
+  // assign decisions
+  for (std::size_t i=0; i < n; ++i)
+    ptr->_vtype.push_back(vtype);
+  for (std::size_t i=0; i < n; ++i)
+    ptr->_lb.push_back(default_lower);
+  for (std::size_t i=0; i < n; ++i)
+    ptr->_ub.push_back(default_upper);
+  // return success
+  return true;
+}
