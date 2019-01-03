479d1dd9e7086c3fc8f9fedecd61ca83eee221a9
jeffreyhanson <jeffrey.hanson@uqconnect.edu.au>
2019-01-03 13:04:54 +1000

    update #2 messages

--- R/zzz.R
+++ R/zzz.R
@@ -6,21 +6,8 @@
 }
 
 .onAttach <- function(libname, pkgname) {
-  # print message if prioritizr already loaded
-  if ("prioritizr" %in% .packages()) {
-    message(
-      "You have loaded both ppr and prioritizr - ",
-      "this is likely to cause serious issues.\n",
-      "You should only have one of these packages loaded at a time,\n",
-      "please unload one ppr or prioritizr using one of the commands below:\n",
-      "  detach(\"package:ppr\", unload = TRUE) # unload ppr package\n",
-      "  detach(\"package:prioritizr\", unload = TRUE) # unload prioritizr",
-      "package\n",
-      "and then reload the desired package."
-    )
-  }
-  # set hook to print message if prioritizr is loaded later on
-  setHook(packageEvent("prioritizr", "attach"), function(...) {
+  # define message generator function
+  msg <- function() {
     packageStartupMessage(cli::rule())
     packageStartupMessage(
       "You have loaded both ppr and prioritizr - ",
@@ -33,5 +20,12 @@
       "and then reload the desired package."
     )
     packageStartupMessage(cli::rule())
+  }
+  # print message if prioritizr already loaded
+  if ("prioritizr" %in% .packages())
+   msg()
+  # set hook to print message if prioritizr is loaded later on
+  setHook(packageEvent("prioritizr", "attach"), function(...) {
+    msg()
   })
 }
