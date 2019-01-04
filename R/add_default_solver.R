d751777bd98874086460bed4e7016558b6093835
jeffreyhanson <jeffrey.hanson@uqconnect.edu.au>
2019-01-04 19:17:35 +1000

    default solver

--- R/add_default_solver.R
+++ R/add_default_solver.R
@@ -23,7 +23,27 @@ NULL
 #' @seealso \code{\link{solvers}}.
 #'
 #' @examples
-#' #TODO
+#' # load data
+#' data(sim_projects, sim_features, sim_actions)
+#'
+#' # build problem with default solver
+#' p <- problem(sim_projects, sim_actions, sim_features,
+#'              "name", "success", "name", "cost", "name") %>%
+#'      add_max_richness_objective(budget = 200) %>%
+#'      add_binary_decisions() %>%
+#'      add_default_solver()
+#'
+#' # print problem
+#' print(p)
+#'
+#' # solve problem
+#' s <- solve(p)
+#'
+#' # print solution
+#' print(s)
+#'
+#' # plot solution
+#' plot(p, s)
 #'
 #' @export
 add_default_solver <- function(x, ...) {
