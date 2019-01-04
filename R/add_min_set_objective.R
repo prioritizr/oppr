02fecc8eac99c38e3bb787d4738bace63ea4f3b7
jeffreyhanson <jeffrey.hanson@uqconnect.edu.au>
2019-01-04 14:58:26 +1000

    add min set example

--- R/add_min_set_objective.R
+++ R/add_min_set_objective.R
@@ -102,7 +102,29 @@ NULL
 #' @inherit add_max_sum_persistence_objective return
 #'
 #' @examples
-#' #TODO
+#' # load the ggplot2 R package to customize plot
+#' library(ggplot2)
+#'
+#' # load data
+#' data(sim_projects, sim_features, sim_actions)
+#'
+#' # build problem with minimum set objective and targets that require each
+#' # feature to have a 30\% chance of persisting into the future
+#' p <- problem(sim_projects, sim_actions, sim_features,
+#'              "name", "success", "name", "cost", "name") %>%
+#'       add_min_set_objective() %>%
+#'       add_absolute_targets(0.3) %>%
+#'       add_binary_decisions()
+#'
+#' # solve problem
+#' s <- solve(p)
+#'
+#' # print solution
+#' print(s)
+#'
+#' # plot solution, and add a dashed line to indicate the feature targets
+#' plot(p, s) +
+#' geom_hline(yintercept = 0.3, linetype = "dashed")
 #'
 #' @name add_min_set_objective
 NULL
