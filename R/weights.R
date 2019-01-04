988924cb5394a14d920e50987492d4075356c0cd
jeffreyhanson <jeffrey.hanson@uqconnect.edu.au>
2019-01-04 16:44:04 +1000

    add weights examples

--- R/weights.R
+++ R/weights.R
@@ -22,7 +22,25 @@ NULL
 #'  \code{\link{solvers}}, \code{\link{targets}}.
 #'
 #' @examples
-#' #TODO
+#' # load data
+#' data(sim_projects, sim_features, sim_actions)
+#'
+#' # build problem with maximum richness objective, $300 budget, and
+#' # feature weights
+#' p <- problem(sim_projects, sim_actions, sim_features,
+#'              "name", "success", "name", "cost", "name") %>%
+#'      add_max_richness_objective(budget = 200) %>%
+#'      add_feature_weights("weight") %>%
+#'      add_binary_decisions()
+#'
+#' # solve problem
+#' s <- solve(p)
+#'
+#' # print solution
+#' print(s)
+#'
+#' # plot solution
+#' plot(p, s)
 #'
 #' @name weights
 NULL
