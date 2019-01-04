17c32c2753e852933b65866d496382c95c4d34e3
jeffreyhanson <jeffrey.hanson@uqconnect.edu.au>
2019-01-04 14:24:51 +1000

    add solve examples

--- R/solve.R
+++ R/solve.R
@@ -51,9 +51,6 @@ NULL
 #' @seealso \code{\link{problem}}, \code{\link{solution_statistics}},
 #'   \code{\link{solvers}}.
 #'
-#' @examples
-#' #TODO
-#'
 #' @name solve
 #'
 #' @importFrom Matrix solve
@@ -62,6 +59,52 @@ NULL
 #'
 #' @aliases solve,OptimizationProblem,Solver-method solve,ProjectProblem,missing-method
 #'
+#' @examples
+#' # load data
+#' data(sim_projects, sim_features, sim_actions)
+#'
+#' # print project data
+#' print(sim_projects)
+#'
+#' # print action data
+#' print(sim_features)
+#'
+#' # print feature data
+#' print(sim_actions)
+#'
+#' # build problem
+#' p <- problem(sim_projects, sim_actions, sim_features,
+#'              "name", "success", "name", "cost", "name") %>%
+#'      add_max_sum_persistence_objective(budget = 400) %>%
+#'      add_feature_weights("weight") %>%
+#'      add_binary_decisions()
+#'
+#' # print problem
+#' print(p)
+#'
+#' \donttest{
+#' # solve problem
+#' s <- solve(p)
+#'
+#' # print output
+#' print(s)
+#'
+#' # print the solver status
+#' print(s$obj)
+#'
+#' # print the objective value
+#' print(s$obj)
+#'
+#' # print the solution cost
+#' print(s$cost)
+#'
+#' # print which actions are funded in the solution
+#' s[, sim_actions$name, drop = FALSE]
+#'
+#' # print the expected probability of persistence for each feature
+#' # if the solution were implemented
+#' s[, sim_features$name, drop = FALSE]
+#' }
 #' @export
 NULL
 
