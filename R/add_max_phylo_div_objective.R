604bd241150c2315ef9d651f9648fe80dc79cefd
jeffreyhanson <jeffrey.hanson@uqconnect.edu.au>
2019-01-04 15:59:35 +1000

    add max phylo examples

--- R/add_max_phylo_div_objective.R
+++ R/add_max_phylo_div_objective.R
@@ -151,8 +151,41 @@ NULL
 #' research & management science}. Springer.
 #'
 #' @examples
-#' #TODO
+#' \donttest{
+#' # load data
+#' data(sim_projects, sim_features, sim_actions, sim_tree)
 #'
+#' # plot tree
+#' plot(sim_tree)
+#'
+#' # build problem with maximum phylogenetic diversity objective and $300 budget
+#' p1 <- problem(sim_projects, sim_actions, sim_features,
+#'              "name", "success", "name", "cost", "name") %>%
+#'       add_max_phylo_div_objective(budget = 300, tree = sim_tree) %>%
+#'       add_binary_decisions()
+#'
+#' # solve problem
+#' s1 <- solve(p1)
+#'
+#' # print solution
+#' print(s1)
+#'
+#' # plot solution
+#' plot(p1, s1)
+#'
+#' # build another problem that includes feature weights
+#' p2 <- p1 %>%
+#'       add_feature_weights("weight")
+#'
+#' # solve problem
+#' s2 <- solve(p2)
+#'
+#' # print solution
+#' print(s2)
+#'
+#' # plot solution
+#' plot(p2, s2)
+#' }
 #' @name add_max_phylo_div_objective
 NULL
 
