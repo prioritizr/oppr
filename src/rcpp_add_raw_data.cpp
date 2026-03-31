1296be4dfdf593b4c71bc2642d4a8a68a7e80a13
Jeffrey Hanson <jeffrey.hanson@uqconnect.edu.au>
2026-03-31 15:41:46 +1300

    try to fix bug with negative sc vars

--- src/rcpp_add_raw_data.cpp
+++ src/rcpp_add_raw_data.cpp
@@ -263,10 +263,8 @@ bool rcpp_add_raw_data(SEXP x, arma::sp_mat pa_matrix, arma::sp_mat pf_matrix,
 
         // slightly expand the range to ensure that they full encompass
         // the range of values that should be interpolated
-        curr_pad_value =
-          std::abs(curr_abs_max_value - curr_abs_min_value) * 0.01;
-        curr_abs_min_value -= curr_pad_value;
-        curr_abs_max_value += curr_pad_value;
+        curr_abs_min_value *= 0.99;
+        curr_abs_max_value *= 1.01;
 
         /// pre-allocate vectors for xy pwl data
         model_pwl_x[p].reserve(n_approx_points);
